a545719e18ffea3ca7d90d4fef92360828ee8c63
wintyo <wintyo1024@gmail.com>
2020-01-05 23:31:43 +0900

    アコーディオンを入れてみる

--- src/typescripts/pages/Test.vue
+++ src/typescripts/pages/Test.vue
@@ -1,17 +1,34 @@
 <template lang="pug">
 div
   p テスト
+  button(@click="$data.isOpen = !$data.isOpen") toggle
+  div
+    SlideUpDown(
+      :active="$data.isOpen"
+    )
+      .content content
   button(@click="$router.back()") back
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+const SlideUpDown = require('vue-slide-up-down').default;
 
 export default Vue.extend({
   name: 'Test',
+  components: {
+    SlideUpDown,
+  },
+  data() {
+    return {
+      isOpen: false,
+    };
+  },
 });
 </script>
 
 <style lang="scss" scoped>
-//
+.content {
+  padding: 10px;
+}
 </style>
